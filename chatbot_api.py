0a3a14738410de61a220f5df8d2e4e03c45e3d97
Izad Imran Tan <izad.mhdborhan@myeg.com.my>
2025-03-27 14:51:23 -0400

    Added test route for checking alive or not

--- chatbot_api.py
+++ chatbot_api.py
@@ -49,6 +49,10 @@ def ask(query: str):
     
     return {"answer": answer}
 
+@app.get("/ping")
+def ping():
+    return {"status": "ok"}
+
 if __name__ == "__main__":
     import uvicorn
     # uvicorn.run(app, host="0.0.0.0", port=8000)
